d5fb72930e87501a8573a0a26af98c01f1559840
trinhan2103la <trinhan212003la@gmail.com>
2024-07-05 11:28:01 +0700

    [Update] code TikTok

---
+++ tiktok/src/assets/images/index.jsx
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import logo from "../../assets/images/logo.svg";
+
+const images = {
+  logo,
+};
+
+export default images;
--- tiktok/src/components/layout/component/Header/index.jsx
+++ tiktok/src/components/layout/component/Header/index.jsx
@@ -1,8 +1,22 @@
+import images from "../../../../assets/images";
+
 export default function Header() {
   return (
     <header className="h-16 shadow-sm shadow-white flex justify-center w-full">
-      <div className="h-full w-[1150px] bg-gray-400 flex justify-center">
-        {/*Logo*/}
+      <div className="h-full w-[1150px]">
+        <div className="logo">
+          <img src={images.logo} alt="tikTok" />
+        </div>
+        <div className="search">
+          <input
+            type="text"
+            placeholder="Search accounts anh videos"
+            spellCheck={false}
+          />
+          <button className="clear-btn">{/* Clear */}</button>
+          {/* Loading */}
+          <button className="search-btn">{/* search */}</button>
+        </div>
         {/*Search*/}
       </div>
     </header>
