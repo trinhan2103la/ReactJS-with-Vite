d3b22fd38d311405a29a76e09641f77933834bbe
trinhan2103la <trinhan212003la@gmail.com>
2024-07-06 09:25:34 +0700

    upp

--- tiktok/src/components/layout/component/Header/index.jsx
+++ tiktok/src/components/layout/component/Header/index.jsx
@@ -8,32 +8,49 @@ import {
   faSpinner,
 } from "@fortawesome/free-solid-svg-icons";
 import Tippy from "@tippyjs/react/headless";
+import { useState, useEffect } from "react";
 
 export default function Header() {
+  const [searchResult, setSearchResult] = useState([]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setSearchResult(1, 2, 2);
+    }, 3000);
+  }, []);
   return (
     <header className="h-16 shadow-sm shadow-white flex justify-center w-full">
       <div className="h-full w-[71.875rem] flex items-center justify-between">
         <div className="logo">
           <img src={images.logo} alt="tikTok" />
         </div>
-        <div className="flex w-[22.5625rem] h-[2.875rem] bg-colorSearch rounded-[5.75rem] pl-4 relative borDer focus:border-colorl">
-          <input
-            type="text"
-            placeholder="Search accounts anh videos"
-            spellCheck={false}
-            className="flex-1 text-colorInput border-none outline-none bg-transparent h-full  caret-red-500 input"
-          />
-          <button className="absoluter pr-2 text-colorlis cursor-default">
-            <FontAwesomeIcon icon={faCircleXmark} className="text-colorlis" />
-          </button>
-          <div className="absoluter pr-2 text-colorlis hidden ">
-            <FontAwesomeIcon icon={faSpinner} />
+        <Tippy
+          interactive
+          render={(attrs) => (
+            <div className="" tabIndex="-1" {...attrs}>
+              Ket Qua
+            </div>
+          )}
+        >
+          <div className="flex w-[22.5625rem] h-[2.875rem] bg-colorSearch rounded-[5.75rem] pl-4 relative border-solid border-transparent border focus-within:border-colorl">
+            <input
+              type="text"
+              placeholder="Search accounts anh videos"
+              spellCheck={false}
+              className="flex-1 text-colorInput border-none outline-none bg-transparent h-full  caret-red-500 input"
+            />
+            <button className="absoluter pr-2 text-colorlis cursor-default">
+              <FontAwesomeIcon icon={faCircleXmark} className="text-colorlis" />
+            </button>
+            <div className="absoluter pr-2 text-colorlis hidden ">
+              <FontAwesomeIcon icon={faSpinner} />
+            </div>
+            <div className="bg-colorl mt-[0.6875rem] w-[0.0625rem] h-6"></div>
+            <button className=" setColor w-[3.25rem] h-full border-none outline-none rounded-r-[5.75rem] hover:bg-colorli text-colorlis">
+              <FontAwesomeIcon icon={faMagnifyingGlass} />
+            </button>
           </div>
-          <div className="bg-colorl mt-[0.6875rem] w-[0.0625rem] h-6"></div>
-          <button className=" setColor w-[3.25rem] h-full border-none outline-none rounded-r-[5.75rem] hover:bg-colorli text-colorlis">
-            <FontAwesomeIcon icon={faMagnifyingGlass} />
-          </button>
-        </div>
+        </Tippy>
         <div>Action</div>
       </div>
     </header>
